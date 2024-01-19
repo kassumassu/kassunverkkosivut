fc34965ccbcbb657c319a912aee5c4b25c8d3f60
kassumassu <142390041+kassumassu@users.noreply.github.com>
2024-01-19 19:33:57 +0200

    Add files via upload

---
+++ pagetwo.js
@@ -0,0 +1,1 @@
+alert("Tervetuloa sivulle kaksi!");
---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Tervetuloa!"); 
+let x = document.addEventListener("keypress", (ev) => {alert("Meillä on koira sen nimi on leo se on tiibetin spanieli!")}); 
+function s() {
+    alert("Hienoa että tykkäät!"); 
+} 
+function k() {
+    alert("Mahtavaa että tykkäät!");
+} 
+function msg() {
+    alert("Pääset sivulle kaksi linkistä kolme!"); 
+}
